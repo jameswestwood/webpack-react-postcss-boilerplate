7f856e318b43344ab4a925dddcb1d18ee150c0b4
james <jameswestwood@protonmail.com>
2018-06-12 23:03:39 +1000

    adjust section vertical sizing when flattened, split off content styling into seperate stylesheet

--- src/js/components/about/about.jsx
+++ src/js/components/about/about.jsx
@@ -7,10 +7,10 @@ class About extends React.Component<Props, State>
 {
   render(){
     return (
-      <React.Fragment>
+      <div className="content">
         <h2>About</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-      </React.Fragment>
+      </div>
     );
   }
 }
--- src/js/components/welcome/welcome.jsx
+++ src/js/components/welcome/welcome.jsx
@@ -3,14 +3,16 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
+import styles from '../../../css/content.css';
+
 class Welcome extends React.Component<Props, State>
 {
   render(){
     return (
-      <React.Fragment>
+      <div className="content">
         <h2>Welcome</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </React.Fragment>
+      </div>
     );
   }
 }
