5bb48063f02c795663aa943c02ca6f0906b3c79e
james <jameswestwood@protonmail.com>
2018-06-13 11:41:16 +1000

    update content styling

--- src/js/components/about/about.jsx
+++ src/js/components/about/about.jsx
@@ -9,7 +9,8 @@ class About extends React.Component<Props, State>
     return (
       <div className="content">
         <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
     );
   }
--- src/js/components/welcome/welcome.jsx
+++ src/js/components/welcome/welcome.jsx
@@ -13,7 +13,8 @@ class Welcome extends React.Component<Props, State>
     return (
       <div className="content">
         <h2>Welcome</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex <a href="#">ea commodo consequat</a>.</p>
         <Button label="primary" type="cta" />
         <Button label="secondary" />
       </div>
