2a671bca067f238cdf642988ac0188ca7c2dc634
james <jameswestwood@protonmail.com>
2018-06-14 21:48:39 +1000

    improve transitions between sections

--- src/js/components/ui.jsx
+++ src/js/components/ui.jsx
@@ -25,6 +25,9 @@ type State = {
 
 class UI extends React.Component<Props, State>
 {
+  currentSection:HTMLElement;
+  transitionDuration:number = 300;
+
   state:State = {
     flat: true
   };
@@ -75,25 +78,46 @@ class UI extends React.Component<Props, State>
     };
   }
 
-  handleEnter = (el:HTMLElement) => {
+  transitionSections = async (nextSection:HTMLElement) => {
+
+    // initially hide new section while old transitions out
     anime({
-      targets: el,
-      opacity: [
-        {value:0, duration:0},
-        {value:1, duration:500, delay:200}
-      ],
+      targets: nextSection,
+      opacity: 0,
+      scale:1.1,
+      duration:0,
+    });
+
+    if(this.currentSection != null)
+    {
+      //  take next section out of page flow temporarily while current section is transitioned out
+      nextSection.style.position = "absolute";
+
+      // transition out current section
+      this.exitComplete = anime({
+                            targets: this.currentSection,
+                            opacity: 0,
+                            scale: 0.9,
+                            easing: 'easeInQuad',
+                            duration: this.transitionDuration
+                          });
+
+      await this.exitComplete.finished;
+
+      this.currentSection.style.position = "absolute";
+      nextSection.style.position = "static";
+    }
+
+    // transition in next section
+    anime({
+      targets: nextSection,
+      opacity: 1,
+      scale: 1,
+      duration: this.transitionDuration,
       easing: 'easeOutQuad'
     });
-  }
 
-  handleExit = (el:HTMLElement) => {
-    anime({
-      targets: el,
-      opacity: [
-        {value:0, duration:200}
-      ],
-      easing: 'easeInQuad'
-    });
+    this.currentSection = nextSection;
   }
 
   render(){
@@ -101,59 +125,55 @@ class UI extends React.Component<Props, State>
       <div className={"ui" + (!this.state.flat ? ' ui--layered' : '')}>
         <Header specifier="ui" paths={this.paths} />
         <main className="ui__content">
-          <div className="ui__container">
-            {(() => {
+        {(() => {
+          switch(this.state.flat)
+          {
+          case false : // render single page app for higher resolutions
 
-              switch(this.state.flat)
-              {
-              case false :
+          return <Route render={({ location }) => (
+                  <TransitionGroup>
+                    <CSSTransition key={location.pathname}
+                                    classNames="ui__section-"
+                                    onEnter={(el:HTMLElement) => {this.transitionSections(el);}}
+                                    timeout={this.transitionDuration}>
+                      <Switch location={location}>
+                        {(() => {
+                          const keys:Array<string> = Object.keys(this.paths);
+                          let routes:Array<React.Element<any>> = [];
 
-              return <Route render={({ location }) => (
-                      <TransitionGroup>
-                        <CSSTransition key={location.pathname}
-                                        classNames="ui__section-"
-                                        onEnter={(el:HTMLElement) => {this.handleEnter(el);}}
-                                        onExit={(el:HTMLElement) => {this.handleExit(el);}}
-                                        timeout={500}>
-                          <Switch location={location}>
-                            {(() => {
-                              const keys:Array<string> = Object.keys(this.paths);
-                              let routes:Array<React.Element<any>> = [];
+                          for(let i:number = 0; i < keys.length; i++)
+                          {
+                            routes.push(<Route exact={true}
+                                               path={this.paths[keys[i]].path}
+                                               render={() => <section className="ui__section">{this.paths[keys[i]].component}</section>}
+                                               key={'route-' + (keys[i])} />);
+                          }
 
-                              for(let i:number = 0; i < keys.length; i++)
-                              {
-                                routes.push(<Route exact={true}
-                                                   path={this.paths[keys[i]].path}
-                                                   render={() => <section className="ui__section">{this.paths[keys[i]].component}</section>}
-                                                   key={'route-' + (keys[i])} />);
-                              }
+                          return routes;
 
-                              return routes;
+                        })()}
+                      </Switch>
+                    </CSSTransition>
+                  </TransitionGroup>
+                )}/>;
 
-                            })()}
-                          </Switch>
-                        </CSSTransition>
-                      </TransitionGroup>
-                    )}/>;
+            case true :  // render flattened app for lower resolutions
 
-              case true :
+            const keys:Array<string> = Object.keys(this.paths);
+            let sections:Array<React$Node> = [];
 
-                const keys:Array<string> = Object.keys(this.paths);
-                let sections:Array<React$Node> = [];
+            for(let i:number = 0; i < keys.length; i++)
+            {
+              let section:React$Node = this.paths[keys[i]].component;
 
-                for(let i:number = 0; i < keys.length; i++)
-                {
-                  let section:React$Node = this.paths[keys[i]].component;
+              sections.push(<section id={keys[i]}
+                                 className="ui__section"
+                                 key={"section-" + (keys[i])}>{section}</section>);
+            }
 
-                  sections.push(<section id={keys[i]}
-                                     className="ui__section"
-                                     key={"section-" + (keys[i])}>{section}</section>);
-                }
-
-                return sections;
-              };
-            })()}
-          </div>
+            return sections;
+          };
+        })()}
         </main>
         <Footer specifier="ui__footer" />
       </div>
